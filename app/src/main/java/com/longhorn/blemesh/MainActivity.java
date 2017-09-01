236ad20572c192214d80c1cd50b08007708e9f08
lighting0675 <zrj244265428@gmail.com>
2017-09-01 16:52:59 +0800

    测试

--- app/src/main/java/com/longhorn/blemesh/MainActivity.java
+++ app/src/main/java/com/longhorn/blemesh/MainActivity.java
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
